2c071a1b7e630a0e29e5de264c92d08982c6c16e
hendo12 <henrydoce@protonmail.com>
2023-05-02 08:56:30 -0500

    useQuery module

--- server/src/schema.js
+++ server/src/schema.js
@@ -3,7 +3,7 @@ const gql = require("graphql-tag");
 const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
-    tracksforHome: [Track!]!
+    tracksForHome: [Track!]!
   }
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
